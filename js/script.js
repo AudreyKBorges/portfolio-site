fe3f0a3af9cf97fd1ce3718ee5e4cf271ccf34ed
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-06-13 18:31:41 -0500

    Started adding styles for hamburger menu and sidebar to show on mobile and tablet

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ window.onscroll = () => {
     const nav = document.querySelector('#main-nav');
     if(this.scrollY <= 75) nav.className = ''; else nav.className = 'scroll';
   };
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
