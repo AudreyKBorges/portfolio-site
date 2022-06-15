5185851274df881e435c69a69c57cfaf0f1adbd0
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-06-15 18:39:38 -0500

    Changed main tag to content
    This was done to avoid confusion and conflict with another main tag.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ window.onscroll = () => {
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("content").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("content").style.marginLeft= "0";
 }
