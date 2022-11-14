95a2893a1ad8287bfd6fa0ff8ac53650d98963f4
Audrey <audreyborges7@gmail.com>
2022-11-14 17:41:02 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,22 +1,31 @@
 <?php
 /**
- * PHP version 7.3
+ * PHP version 8.08
  * 
  * @category None
  * @package  None
- * @author   "Audrey Borges <audrey@audreyborges.com>
+ * @author   "Audrey Borges <audrey.borges@audreyborges.com>
  * @license  https://www.audreyborges.com/ GNU General Public License
  * @link     https://www.audreyborges.com/
  */
+
 $servername = "localhost";
-$username = "1142513";
-$password = "59WR.D!tgXBYK.D";
+$username = "dbu3360126";
+$password = "5eK6WHwqT9j@46q";
+$dbname = "audreyborges.com";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+
+$sql = "INSERT INTO `CONTACTS`(`id`, `name`, `email`, `details`) VALUES (Billy Bob, billy.bob@gmail.com, Hi)";
+
+if ($conn->query($sql) === true) {
+    echo "Record inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
