b5cb9190dc0ce1e5b84a8f798012efe1ed609fc9
Audrey <audreyborges7@gmail.com>
2023-10-23 21:55:40 -0500

    Footer now spans the bottom of the page.

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * PHP version 8.08
  * 
@@ -9,18 +10,22 @@
  * @link     https://www.audreyborges.com/
  */
 ?>
-<footer class="main-footer">A-B WebDev copyright &copy; <script>document.write(new Date().getFullYear())</script></footer>
-                    </div>
-                </div>
-                </div> <!-- End content  -->
-            <script src="./scripts/script.js"></script>
-            <!-- Google reCAPTCHA API -->
-            <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-            <!-- Animate on Scroll API -->
-            <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-            <script>
-                // Animate on Scroll function call
-                AOS.init();
-            </script>
-    </body>
+</div>
+</div>
+</div> <!-- End content  -->
+<footer class="main-footer">A-B WebDev copyright &copy; <script>
+        document.write(new Date().getFullYear())
+    </script>
+</footer>
+<script src="./scripts/script.js"></script>
+<!-- Google reCAPTCHA API -->
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<!-- Animate on Scroll API -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    // Animate on Scroll function call
+    AOS.init();
+</script>
+</body>
+
 </html>
